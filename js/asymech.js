7943b4d13fc22fef9e3b42675470c707549f6479
Julius Leon Quasebarth <feldrandstudios@gmail.com>
2015-04-07 00:54:13 +0200

    Beitragen-Seite verbessert
    Neben dieser Veränderung wurden auf dem Home-Bildschirm neue Links
    hinzugefügt, eine Nachricht "Diese Funktion ist im Moment nicht
    unterstützt", leichte Stilveränderungen und die Client-Version für das
    Hinzufügen von Büchern.

--- js/asymech.js
+++ js/asymech.js
@@ -22,6 +22,33 @@ function load() {
     });
 }
 
+function sendNewBook() {
+    var dataToBeSent = {};
+    dataToBeSent.name = $('#bname>input').val();
+    dataToBeSent.autor = $('#bautor>input').val();
+    for (var i = 1; i <= 13; i++) {
+        dataToBeSent["c" + i] = $('#c' + i + '>input').val();
+    }
+    console.log(dataToBeSent);
+    $.ajax({
+        method: "POST",
+        url: "cgi-bin/addBook",
+        data: dataToBeSent,
+        contentType: "text/plain",
+        success: function(content, status, jqXHR) {
+            switch(content) {
+                case "S": notify("success", "Das Buch wurde hinzugefügt!"); break;
+                case "R": notify("success", "Deine Eingabedaten wurden mit den bereits vorhandenen gemischt!"); break;
+                case "P": notify("warning", "Es entstand ein Fehler beim Verarbeiten der Daten, wahrscheinlich weil einige Zahlen nicht richtig eingegeben wurden."); break;
+                case "E": notify("danger", "Das Buch konnte aus serverseitigen Gründen nicht hinzugefügt werden."); break;
+            }
+        },
+        error: function(jqXHR, status, error) {
+            notify("danger", "Die Daten konnten aus (wahrscheinlich) verbindungstechnischen Gründen nicht hinzugefügt.");
+        }
+    });
+}
+
 function notify(type, msg) {
     d=document.createElement('div');
     $(d).addClass("alert alert-" + type)
