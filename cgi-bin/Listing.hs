d4ea5bb92fc94a0c8737682bd073f3ec4c32ac2e
Julius Quasebarth <feldrandstudios@gmail.com>
2015-07-13 19:49:55 +0200

    Modify Listing.hs, Add helper functions

--- cgi-bin/Listing.hs
+++ cgi-bin/Listing.hs
@@ -11,6 +11,7 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.ByteString.UTF8 as BSU
 import Data.Convertible.Base
+import Data.Char
 import Data.Maybe
 import Data.URLEncoded
 import Data.List
@@ -26,11 +27,11 @@ main = do {
 	putStr "Content-Type: text/html; charset=utf8\n\n";
 	s <- getLine;
 	let {p = either (const [("name","")]) pairs (importString s :: Either String URLEncoded)};
-	let {f = find ((==) "name" . fst) p};
-	when (isNothing f) $ putStr "P" >> exitFailure;
-	l <- getFirstMatchingBooks 20 (snd $ fromJust f) conn;
-	let {inter = BSC.unlines l};
-	if (isUTF8Encoded . BSC.unpack) inter then putStr $ decodeString $ BSC.unpack inter else putStr $ BSC.unpack inter;
+	let {f = fromJust $ find ((==) "name" . fst) p};
+	l <- getFirstMatchingBooks 50 (snd f) conn;
+	let {inter = map (\x -> if isUTF8Encoded $ BSC.unpack x then decodeString $ BSC.unpack x else BSC.unpack x) l};
+	putStr $ unlines inter;
+	exitSuccess;
 	}
 
 getFirstMatchingBooks :: IConnection conn => Int -> String -> conn -> IO [ByteString]
